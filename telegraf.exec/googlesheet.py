bac9d8ae8243f7cfa66e0fc8acdd92ffb8b03852
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2025-01-29 19:36:55 +0900

    minor change.

--- telegraf.exec/googlesheet.py
+++ telegraf.exec/googlesheet.py
@@ -7,15 +7,59 @@ SERVICE_ACCOUNT_FILE = '/misc/software/slow-monitor/key/hyps-project-8c7f1d11b31
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
 credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+client = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1VBGUxsFfQNpFoOjkS36u_2WvrtppF8zwodLceM9touk/edit'
+spreadsheet = client.open_by_url(spreadsheet_url)
+worksheet = spreadsheet.sheet1
+
+def set_protection(start_column, end_column):
+  protect_request = {
+    "requests": [
+      {
+        "addProtectedRange": {
+          "protectedRange": {
+            "range": {
+              "sheetId": worksheet.id,
+              "startRowIndex": 0,
+              "startColumnIndex": start_column,
+              "endColumnIndex": end_column
+            },
+            "description": "Protect entire sheet",
+            "warningOnly": False  # uneditable
+          }
+        }
+      }
+    ]
+  }
+  spreadsheet.batch_update(protect_request)
+
+def request_format_number(start_column, end_column, pattern):
+  requests = {
+    "requests": [
+      {
+        "repeatCell": {
+          "range": {
+            "sheetId": worksheet.id,
+            "startRowIndex": 1,
+            "startColumnIndex": start_column,
+            "endColumnIndex": end_column
+          },
+          "cell": {
+            "userEnteredFormat": {
+              "numberFormat": {
+                "type": "NUMBER",
+                "pattern": pattern
+              }
+            }
+          },
+          "fields": "userEnteredFormat.numberFormat"
+        }
+      }
+    ]
+  }
+  spreadsheet.batch_update(requests)
 
 def update():
-  client = gspread.authorize(credentials)
-
-  spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1VBGUxsFfQNpFoOjkS36u_2WvrtppF8zwodLceM9touk/edit'
-  spreadsheet = client.open_by_url(spreadsheet_url)
-
-  worksheet = spreadsheet.sheet1
-
   csv_file = '/misc/subdata/runsummary.csv'
   with open(csv_file, mode="r") as file:
     reader = csv.reader(file)
@@ -31,59 +75,11 @@ def update():
           except ValueError:
             processed_row.append(cell)
       data.append(processed_row)
-  # worksheet.clear()
   worksheet.update(data, 'A1')
-
-# requests = {
-#   "requests": [
-#     {
-#       "updateSheetProperties": {
-#         "properties": {
-#           "sheetId": worksheet.id,
-#           "gridProperties": {
-#             "frozenRowCount": 1,
-#             "frozenColumnCount": 1
-#           }
-#         },
-#         "fields": "gridProperties.frozenRowCount,gridProperties.frozenColumnCount"
-#       }
-#     },
-#     {
-#       "addConditionalFormatRule": {
-#         "rule": {
-#           "ranges": [
-#             {
-#               "sheetId": worksheet.id,
-#               "startRowIndex": 0,  # 0ベースで指定（ヘッダーを含む）
-#               "startColumnIndex": 0,  # 行全体の開始（A列）
-#               "endColumnIndex": worksheet.col_count  # 最終列まで
-#             }
-#           ],
-#           "booleanRule": {
-#             "condition": {
-#               "type": "CUSTOM_FORMULA",
-#               "values": [
-#                 {
-#                   # "userEnteredValue": '=AND(ISNUMBER(SEARCH("production", $D1)), NOT(ISNUMBER(SEARCH("check", $D1))), NOT(ISNUMBER(SEARCH("test", $D1))))'
-#                   # "userEnteredValue": '=AND(ISNUMBER(SEARCH("cosmic", $D1)), NOT(ISNUMBER(SEARCH("check", $D1))), NOT(ISNUMBER(SEARCH("test", $D1))))'
-#                   # "userEnteredValue": '=AND(ISNUMBER(SEARCH("pedestal", $D1)), NOT(ISNUMBER(SEARCH("check", $D1))), NOT(ISNUMBER(SEARCH("test", $D1))))'
-#                   "userEnteredValue": '=AND(ISNUMBER(SEARCH("study", $D1)), NOT(ISNUMBER(SEARCH("check", $D1))), NOT(ISNUMBER(SEARCH("test", $D1))))'
-#                 }
-#               ]
-#             },
-#             "format": {
-#               "backgroundColor": {
-#                 "red": 0.3,   # ピンク色
-#                 "green": 0.8,
-#                 "blue": 0.4
-#               }
-#             }
-#           }
-#         },
-#         "index": 0  # ルールの優先順位
-#       }
-#     }
-#   ]
-# }
-
-# spreadsheet.batch_update(requests)
+  request_format_number(start_column=4, end_column=6, pattern='#,##0')
+  request_format_number(start_column=6, end_column=7, pattern='0.00')
+  request_format_number(start_column=7, end_column=8, pattern='#,##0')
+  request_format_number(start_column=8, end_column=9, pattern='0.000000')
+  request_format_number(start_column=9, end_column=10, pattern='#,##0')
+  request_format_number(start_column=10, end_column=13, pattern='0.000000')
+  # set_protection(start_column=0, end_column=32)
